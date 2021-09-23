56f9ba7c4d1fcf812ff25f3f4acbee5fe6627141
Medvedev Semyon <61358874+Simon-developer@users.noreply.github.com>
2021-09-23 20:53:05 +0200

    step 4

--- Initial Project/ShareExample/ShareViewController.swift
+++ Initial Project/ShareExample/ShareViewController.swift
@@ -6,25 +6,68 @@
 //
 
 import UIKit
-import Social
 
-class ShareViewController: SLComposeServiceViewController {
+@objc(ShareViewController)
+class ShareViewController: UIViewController {
 
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
+    private let titleLabel = UILabel()
+    private let previewsTable = UITableView()
+    private let confirmButton = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        configureTitle()
+        configureTable()
+        configureConfirmButton()
+        setupLayout()
+    }
+}
+
+// MARK: - Layout
+private extension ShareViewController {
+    func configureTitle() {
+        titleLabel.text = "Загрузка файлов"
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        titleLabel.textAlignment = .center
     }
 
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-    
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    func configureTable() {
+
     }
 
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
+    func configureConfirmButton() {
+        confirmButton.setTitle("Загрузить", for: .normal)
+        confirmButton.backgroundColor = .systemTeal
+        confirmButton.layer.cornerRadius = 14
+        confirmButton.setTitleColor(.white, for: .normal)
     }
 
+    func setupLayout() {
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 14),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+
+        view.addSubview(confirmButton)
+        confirmButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            confirmButton.heightAnchor.constraint(equalToConstant: 56),
+            confirmButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 14),
+            confirmButton.bottomAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.bottomAnchor, multiplier: 1.0),
+            confirmButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -14)
+        ])
+
+        view.addSubview(previewsTable)
+        previewsTable.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            previewsTable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 14),
+            previewsTable.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 24),
+            previewsTable.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -14),
+            previewsTable.bottomAnchor.constraint(equalTo: confirmButton.topAnchor, constant: -24)
+        ])
+    }
 }
